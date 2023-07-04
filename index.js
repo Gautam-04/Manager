0f5290852e08516614ab959b003df189866a74c9
Gautam-666 <2022.gautam.rai@ves.ac.in>
2023-07-04 16:48:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(process.env.BOTTOKEN);
+
+client.on('ready', readyDiscord);
+
+function readyDiscord(){
+    console.log("Hello")
+}
